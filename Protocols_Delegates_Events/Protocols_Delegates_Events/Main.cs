6ed89cc81f24ae3a5b6aa13525264f442b4c25ba
HTC Vive <HTC Vive@DESKTOP-QG75O18>
2017-12-23 19:16:50 +1030

    Test

--- Protocols_Delegates_Events/Protocols_Delegates_Events/Main.cs
+++ Protocols_Delegates_Events/Protocols_Delegates_Events/Main.cs
@@ -9,7 +9,7 @@ namespace Protocols_Delegates_Events
 {
 	public class Application
 	{
-		// This is the main entry point of the application.
+		// This is the main entry point of the application..
 		static void Main (string[] args)
 		{
 			// if you want to use a different Application Delegate class from "AppDelegate"
